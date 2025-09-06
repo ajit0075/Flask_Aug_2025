3b2ed00ee3257e952b360a03fb1cb4ad89d274e2
ajit0075 <84534537+ajit0075@users.noreply.github.com>
2025-09-06 20:48:31 +0530

    web app code

--- loan.py
+++ loan.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask , request , jsonify
+import pickle
 
 app= Flask(__name__)
 
+with open('classifier.pkl', 'rb') as f :
+    model= pickle.load(f)
+
+
+ 
 @app.route("/", methods =['GET'])
 def hell():
     return "<h1> Loan app Application </h1>"
@@ -10,3 +16,35 @@ def hell():
 def pred():
     return "<h1> I will make pridict</h1>"
 
+@app.route("/predict" , methods =['POST'])
+def predict_post():
+    loan_req= request.get_json()
+    print(loan_req)
+    
+    if  loan_req["Gender"]=="Male":
+        Gender = 0
+    else:
+        Gender =0
+    
+    if loan_req["Married"] =="No":
+        Married = 0
+    else:
+        Married=1
+
+    ApplicationIncome = loan_req["ApplicationIncome"]  
+    LoanAmount = loan_req["LoanAmount"]  
+    credict_histroy = loan_req["credicHistory"]
+
+    input_data = [Gender,Married,ApplicationIncome,LoanAmount,credict_histroy ]
+    print(input_data)
+    result= model.predict([input_data])
+
+    if  result[0]== 1 :
+        pred= "Approved "
+    else:
+        pred = "Rejected"
+    
+    return jsonify({"Loan approval status is ":pred})
+    
+    
+    #return jsonify({'prediction': result.tolist()}) if we dont use if else of predict
